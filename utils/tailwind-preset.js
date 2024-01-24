4400f4aec12f4f6a50c6729781bd2d36702daf95
Rub3n18 <155134745+Rub3n18@users.noreply.github.com>
2024-01-23 22:40:00 -0500

    Update tailwind-preset.js

--- utils/tailwind-preset.js
+++ utils/tailwind-preset.js
@@ -52,8 +52,8 @@ module.exports = {
       theme: {
         bejamas: {
           colors: {
-            primary: '#FF8585',
-            'gradient-1': '#7d7aff',
+            primary: '#5EEFC4',
+            'gradient-1': '#FFC700',
             'gradient-2': '#2121E2',
             'gradient-3': '#FF76B8',
             'gradient-4': '#001AFF',
