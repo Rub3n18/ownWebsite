6745f4088beb07b85673e12751d0fa3d2f85895c
Rub3n18 <155134745+Rub3n18@users.noreply.github.com>
2024-01-29 23:11:40 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,11 @@
 export default function(){
   return <nav className="nav">
     <a href="#" className="site-title">Site Name</a>
-    
+    <ul class="menu">
+        <li><a href="#experience">My dashboard</a></li>
+        <li><a href="#project">My dashboard</a></li>
+        <li><a href="#volunteer">My dashboard</a></li>
+     
+    </ul>
     </nav>
 }
