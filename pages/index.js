d9464422a30ba9c7c7a1d30f6171a401cf7eb95a
Rub3n18 <155134745+Rub3n18@users.noreply.github.com>
2024-01-31 23:16:44 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,37 +26,8 @@ export default function Index({ posts, globalData }) {
   <h3 id="experience" className="text-9x3 text-left"><b>Experience</b></h3>
   <h3 id="project" className="text-9x3 text-left"><b>Projects</b></h3>
   <h3 id="volunteer" className="text-9x3 text-left"><b>Volunteering</b></h3>'
-    </ul>
   <p className="mt-3 text-lg"></p>
   </div>
-          // {posts.map((post) => (
-          //   <li
-          //     key={post.filePath}
-          //     className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-          //   >
-          //     <Link
-          //       as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
-          //       href={`/posts/[slug]`}
-          //     >
-          //       <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-          //         {post.data.date && (
-          //           <p className="uppercase mb-3 font-bold opacity-60">
-          //             {post.data.date}
-          //           </p>
-                  
-          //         )}
-                  
-          //         <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
-          //         {post.data.description && (
-          //           <p className="mt-3 text-lg opacity-60">
-          //             {post.data.description}
-          //           </p>
-          //         )}
-          //         <ArrowIcon className="mt-4" />
-          //       </a>
-          //     </Link>
-          //   </li>
-          // ))}
         </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
