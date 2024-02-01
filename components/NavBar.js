177d04c717723245fd2d7dbd2cbfa27e68bd65d4
Rub3n18 <155134745+Rub3n18@users.noreply.github.com>
2024-02-01 14:18:09 -0500

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,7 @@
 import house from '..components/house.png'
 export default function NavBar(){
   return <nav className="nav">
-    <a href="#" className="site-title">{house}</a>
+    <a href="#" className="site-title"><img src={house alt =""House}/></a>
     
     <ul class="menu">
       
