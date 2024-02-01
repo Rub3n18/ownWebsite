9c0f5573716268099a22d611273314a28c5d1ced
Rub3n18 <155134745+Rub3n18@users.noreply.github.com>
2024-01-31 23:13:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ posts, globalData }) {
     <Layout>
     <NavBar title={globalData.name}>Jose Perez
     </NavBar>
-      // <SEO title={globalData.name} description={globalData.blogTitle} />
+      <SEO title={globalData.name} description={globalData.blogTitle} />
       <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
